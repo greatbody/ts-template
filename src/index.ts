ab06de4c5b534c888b0350006eee0ca9168be560
greatbody <sunruicode@gmail.com>
2023-03-14 15:41:52 +0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+class Roy {
+    name: string
+    age: number
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    play = () => {
+        console.log(`Hi, I am ${this.name}, I am ${this.age} years old`)
+    }
+}
+
+const roy = new Roy("Roy", 57);
+
+roy.play();
